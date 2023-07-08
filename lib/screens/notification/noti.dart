fbd04ffcf859f1f66793029fb9e89216d2b9c6b5
fawad kaleem <fwdkaleem@gmail.com>
2023-07-08 23:40:27 +0500

    Notification UI Ready

--- lib/screens/notification/noti.dart
+++ lib/screens/notification/noti.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:irish_driving_admin/widgets/colors.dart';
 
 class Noti extends StatefulWidget {
   const Noti({super.key});
@@ -10,6 +11,38 @@ class Noti extends StatefulWidget {
 class _NotiState extends State<Noti> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mainColor,
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text(
+            "Notifications",
+            style: TextStyle(color: colorWhite),
+          ),
+        ),
+        body: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return const Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage("assets/pic.png"),
+                    ),
+                    trailing: Text(
+                      "View",
+                      style: TextStyle(color: mainColor, fontSize: 15),
+                    ),
+                    title: Text(
+                      "User Make the Order Request",
+                      style: TextStyle(color: colorBlack, fontSize: 16),
+                    ),
+                  ),
+                  Divider()
+                ],
+              );
+            }),
+        backgroundColor: colorWhite);
   }
 }
