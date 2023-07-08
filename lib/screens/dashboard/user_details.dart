da392b29d14c8cecd4e412b5a42f1bfca3d1e0f9
fawad kaleem <fwdkaleem@gmail.com>
2023-07-08 23:38:47 +0500

    Registered UI Ready

--- lib/screens/dashboard/user_details.dart
+++ lib/screens/dashboard/user_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:irish_driving_admin/widgets/colors.dart';
 
 class UserDetails extends StatefulWidget {
   const UserDetails({super.key});
@@ -10,6 +11,45 @@ class UserDetails extends StatefulWidget {
 class _UserDetailsState extends State<UserDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mainColor,
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text(
+            "Registered Users",
+            style: TextStyle(color: colorWhite),
+          ),
+        ),
+        body: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return const Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage("assets/pic.png"),
+                    ),
+                    trailing: Text(
+                      "View",
+                      style: TextStyle(color: mainColor, fontSize: 15),
+                    ),
+                    title: Text(
+                      "FawadKaleem",
+                      style: TextStyle(color: colorBlack, fontSize: 16),
+                    ),
+                    subtitle: Text(
+                      "245 B Eden Garden Lahore",
+                      style: TextStyle(
+                          color: colorBlack,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Divider()
+                ],
+              );
+            }),
+        backgroundColor: colorWhite);
   }
 }
