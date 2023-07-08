4417346128e1288d6409bb404748e5b8f6c4966e
fawad kaleem <fwdkaleem@gmail.com>
2023-07-08 18:03:41 +0500

    Login System Ready

--- lib/screens/auth_screen/login_screen.dart
+++ lib/screens/auth_screen/login_screen.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:irish_driving_admin/screens/dashboard/main_screen.dart';
 import 'package:irish_driving_admin/widgets/buttons.dart';
 import 'package:irish_driving_admin/widgets/colors.dart';
 import 'package:irish_driving_admin/widgets/textfield.dart';
+import 'package:irish_driving_admin/widgets/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,6 +17,7 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
   bool _obscureText = true;
+  bool _isLoading = false;
   void _toggle() {
     setState(() {
       _obscureText = !_obscureText;
@@ -55,10 +59,45 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           Container(
             margin: EdgeInsets.only(top: 10, left: 15, right: 15),
-            child: JoinButton(
-              onPressed: () {},
-              title: "Login",
-            ),
+            child: _isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : JoinButton(
+                    onPressed: () {
+                      if (emailController.text.isEmpty &&
+                          passController.text.isEmpty) {
+                        showSnakBar("Both Fields are Required", context);
+                      } else if (passController.text.isEmpty) {
+                        showSnakBar("Password Field are Required", context);
+                      } else if (passController.text.length < 5) {
+                        showSnakBar("Password minimum 5 charaters", context);
+                      } else if (emailController.text != 'admin@gmail.com' &&
+                          passController.text != '123456') {
+                        showSnakBar("Email and password is wrong", context);
+                      } else if (emailController.text == 'admin@gmail.com' &&
+                          passController.text == '123456') {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                        FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passController.text,
+                        );
+                        setState(() {
+                          _isLoading = false;
+                        });
+
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (builder) => MainScreen()));
+                      } else if (emailController.text != 'admin@gmail.com') {
+                        showSnakBar("Email is wrong", context);
+                      } else if (passController != '123456') {
+                        showSnakBar("Password is wrong", context);
+                      }
+                    },
+                    title: "Login",
+                  ),
           )
         ],
       ),
