bbb4981aaf89f2a5075e5f72f2f3cc4d6b1f813b
fawad kaleem <fwdkaleem@gmail.com>
2023-07-16 09:53:57 +0500

    Notification Done

--- lib/screens/notification/noti.dart
+++ lib/screens/notification/noti.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:irish_driving_admin/widgets/colors.dart';
+import 'package:irish_driving_admin/widgets/mywidget.dart';
 
 class Noti extends StatefulWidget {
   const Noti({super.key});
@@ -13,36 +16,142 @@ class _NotiState extends State<Noti> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: mainColor,
-          automaticallyImplyLeading: false,
           centerTitle: true,
+          iconTheme: IconThemeData(color: colorWhite),
+          backgroundColor: mainColor,
           title: const Text(
             "Notifications",
             style: TextStyle(color: colorWhite),
           ),
         ),
-        body: ListView.builder(
-            itemCount: 5,
-            itemBuilder: (BuildContext context, int index) {
-              return const Column(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage("assets/pic.png"),
-                    ),
-                    trailing: Text(
-                      "View",
-                      style: TextStyle(color: mainColor, fontSize: 15),
-                    ),
-                    title: Text(
-                      "User Make the Order Request",
-                      style: TextStyle(color: colorBlack, fontSize: 16),
-                    ),
-                  ),
-                  Divider()
-                ],
-              );
-            }),
-        backgroundColor: colorWhite);
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("appointment")
+                    .where("status", isEqualTo: "start")
+                    .snapshots(),
+                builder: (context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final DocumentSnapshot documentSnapshot =
+                            snapshot.data!.docs[index];
+
+                        return Card(
+                          child: ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Test Center Name:",
+                                  style: TextStyle(
+                                      color: colorBlack,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(documentSnapshot['centerName']),
+                              ],
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Test Center Location:",
+                                  style: TextStyle(
+                                      color: colorBlack,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(documentSnapshot['centerLocation']),
+                              ],
+                            ),
+                            trailing: TextButton(
+                                onPressed: () {
+                                  showDialog<void>(
+                                    context: context,
+                                    barrierDismissible:
+                                        false, // user must tap button!
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('Appointment Detail'),
+                                        content: SingleChildScrollView(
+                                          child: ListBody(
+                                            children: <Widget>[
+                                              const Text(
+                                                'Name',
+                                                style: TextStyle(
+                                                    color: colorBlack,
+                                                    fontWeight:
+                                                        FontWeight.w700),
+                                              ),
+                                              Text(documentSnapshot['name']),
+                                              const Divider(),
+                                              const Text(
+                                                'Booking Date',
+                                                style: TextStyle(
+                                                    color: colorBlack,
+                                                    fontWeight:
+                                                        FontWeight.w700),
+                                              ),
+                                              Text(documentSnapshot['date']),
+                                              const Divider(),
+                                              const Text(
+                                                'Driving Route',
+                                                style: TextStyle(
+                                                    color: colorBlack,
+                                                    fontWeight:
+                                                        FontWeight.w700),
+                                              ),
+                                              InkWell(
+                                                  onTap: () {
+                                                    Navigator.push(
+                                                        context,
+                                                        MaterialPageRoute(
+                                                            builder: (builder) => MyWidget(
+                                                                title: documentSnapshot[
+                                                                    "centerName"],
+                                                                url: documentSnapshot[
+                                                                    'weblink'])));
+                                                  },
+                                                  child: Text(
+                                                    documentSnapshot['weblink'],
+                                                    style: TextStyle(
+                                                        color: blueColor),
+                                                  )),
+                                              const Divider()
+                                            ],
+                                          ),
+                                        ),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: const Text(
+                                              'Close',
+                                              style:
+                                                  TextStyle(color: colorBlack),
+                                            ),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: const Text(
+                                  "View Detail",
+                                  style: TextStyle(color: colorBlack),
+                                )),
+                          ),
+                        );
+                      });
+                })));
   }
 }
--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -5,3 +5,4 @@ const colorGreen = Colors.green;
 const colorBlack = Colors.black;
 const mainColor = Color(0xff00A79D);
 const redColor = Colors.red;
+const blueColor = Colors.blue;
